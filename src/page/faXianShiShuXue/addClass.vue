c4b5b9889ffad43567e5338b2ff9330e9bdc1331
zhangjinn <zhangjinn92@163.com>
2019-04-16 10:47:47 +0800

    changbyzjj

--- src/page/faXianShiShuXue/addClass.vue
+++ src/page/faXianShiShuXue/addClass.vue
@@ -48,7 +48,7 @@
                     </span>
                     <span>
                         <el-button size="mini" @click="editClass">编辑</el-button>
-                        <el-button size="mini" @click="saveClass">保存hhhhhh</el-button>
+                        <el-button size="mini" @click="saveClass">保存hhhghhh</el-button>
                     </span>
                   </div>
                 </el-form-item>
